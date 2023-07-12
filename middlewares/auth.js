df399c7c35c87a59fbbbce8a7a54bac9e3665682
Manish Maharjan <maharjanm96@gmail.com>
2023-07-12 15:00:41 +0545

    Middlewares Added

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,9 +1,17 @@
 const jwt = require("jsonwebtoken");
 
-module.exports=(req,res) =>{
+module.exports = (req, res) => {
     const token = req.header("x-auth-token");
-    if(!token)
-    return res.status(400).send({message:"Access denied, No Token Provided.0"});
+    if (!token)
+        return res.status(400).send({ message: "Access denied, No Token Provided.0" });
 
-    jwt.verify
+    jwt.verify(token, process.env.JWTPRIVATEKEY, (error, validToken) => {
+        if (error) {
+            return res.status(400).send({ message: "Invalid Token" });
+
+        } else {
+            req.user = validToken;
+            next();
+        }
+    })
 }
