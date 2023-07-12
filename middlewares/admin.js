92fd445dacf3192c7fa8d3474db989e05a02aae9
Manish Maharjan <maharjanm96@gmail.com>
2023-07-12 16:38:44 +0545

    User Routes Added And Checked

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = (req, res) => {
+module.exports = (req, res, next) => {
     const token = req.header("x-auth-token");
     if (!token)
         return res.status(400).send({ message: "Access denied, No Token Provided.0" });
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const { User, validate } = require("../models/user");
 const bcrypt = require("bcrypt");
+const auth = require('../middlewares/auth');
+const admin = require('../middlewares/admin');
+const validObjectId = require('../middlewares/validObjectId')
 
 //create user
 router.post("/", async (req, res) => {
@@ -14,14 +17,44 @@ router.post("/", async (req, res) => {
     const salt = await bcrypt.genSalt(Number(process.env.SALT));
     const hashPassword = await bcrypt.hash(req.body.password, salt);
     let newUser = await new User({
-            ...req.body,
-            password: hashPassword
-        }).save();
+        ...req.body,
+        password: hashPassword
+    }).save();
 
-        newUser.password = undefined;
-        newUser.__v = undefined;
+    newUser.password = undefined;
+    newUser.__v = undefined;
 
-            res.status(200).send({data:newUser,message:"Account Created Successfully."})
+    res.status(200).send({ data: newUser, message: "Account Created Successfully." })
 })
+
+//GET ALL USERS
+router.get("/", admin, async (req, res) => {
+    const users = await User.find().select("-password -__v");
+    res.status(200).send({ data: users });
+})
+
+//GET USER BY ID
+router.get("/:id", [validObjectId, auth], async (req, res) => {
+    const user = await User.findById(req.params.id).select("-password -__v");
+    res.status(200).send({ data: user });
+})
+
+//UPDATE USER BY ID
+router.put('/:id', [validObjectId, auth], async (req, res) => {
+    const user = await User.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        { new: true }
+
+    ).select("-password -__v");
+    res.status(200).send({ data: user })
+})
+
+//DELETE User By ID
+router.delete('/:id', [validObjectId,admin], async (req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).send({ message: "Successfully Deleted User." })
+});
+
 
 module.exports = router;
