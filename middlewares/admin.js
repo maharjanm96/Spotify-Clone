5105fae8ec4455b611710fd6c897e20ffe3f7fc8
Manish Maharjan <maharjanm96@gmail.com>
2023-07-13 23:02:19 +0545

    Middleware Access Token

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
     const token = req.header("x-auth-token");
     if (!token)
-        return res.status(400).send({ message: "Access denied, No Token Provided." });
+        return res.status(400).send({ message: "Access Denied, No Token Provided." });
 
     jwt.verify(token, process.env.JWTPRIVATEKEY, (error, validToken) => {
         if (error) {
